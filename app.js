f905240bfda00480c6e77c7704b93b39f664c1c2
Subhaprakash Nayak <subhaprakash@Subhaprakashs-MacBook-Air.local>
2024-03-21 13:08:11 +0530

    day-3

--- app.js
+++
@@ -1,7 +0,0 @@
-console.log('Welcomes to TS');
-console.log('Welcomes to TS');
-var add = function (num1, num2) {
-    return num1 + num2;
-};
-var result = add(19, 28.9);
-console.log('adding numbers', result);
